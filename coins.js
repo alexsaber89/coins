3d10429eff415b6149d50877daf4cfb8918800f2
Alex Saber <Alex Saber>
2016-07-30 12:42:43 -0500

    attempted using modulo to solve, will try different approach on separate branch

--- coins.js
+++ coins.js
@@ -13,14 +13,48 @@
 
 */
 
-function coinCounter () {
+function coinCounter (dollarAmount) {
   // Initialize a JavaScript object to hold the coins
+  var workingDollarAmount = dollarAmount * 100;
   var coinPurse = {};
+  var change;
 
-  coinPurse.quarters = 0;
+  if (workingDollarAmount % 25 === 0) {
+    coinPurse.quarters = workingDollarAmount / 25;
+    console.log("Quarters: " + coinPurse.quarters);
+  } else {
+    coinPurse.quarters = workingDollarAmount / 25;
+    console.log("Quarters: " + coinPurse.quarters);
+    change = workingDollarAmount % 25; //gives the remainder of change
+    console.log("Change: ",change);
+  };
 
-  return coinPurse;
+  // if (change % 10 === 0) {
+  //   coinPurse.dimes = change / 10;
+  //   console.log("Dimes: " + coinPurse.dimes);
+  // } else {
+  //   coinPurse.dimes = change / 10;
+  //   console.log("Dimes: " + coinPurse.dimes);
+  //   change = change % 10; //gives the remainder of change
+  //   console.log("Change: ",change);
+  // };
+
+  // if (change % 5 === 0) {
+  //   coinPurse.nickels = change / 5;
+  //   console.log("Nickels: " + coinPurse.nickels);
+  // } else {
+  //   coinPurse.nickels = change / 5;
+  //   console.log("Nickels: " + coinPurse.nickels);
+  //   change = change % 5; //gives the remainder of change
+  //   console.log("Change: ",change);
+  // };
+
+  // coinPurse.pennies = change;
+
+  // return coinPurse;
 }
 
-var coins = coinCounter()
-console.log();
+console.log(coinCounter(.60));
+
+// var coins = coinCounter()
+// console.log();
